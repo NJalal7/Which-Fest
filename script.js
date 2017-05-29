8d044956c496910010f17ff54d3d576a55f56ab9
Nahrin Jalal <jalal.nahrin@gmail.com>
2017-05-29 16:40:37 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,278 @@
+var festivals = {
+	hipHop: [
+		{
+			name: 'OVO',
+			price: "$$",
+			video:"https://www.youtube.com/embed/1yeOjly6qA0",
+			description:"Every summer, Drake--the world-renowned rapper with an inarguably cultish following--puts on his very own music festival for his adoring fans. Named after the star's record label, OVO Sound, OVO Fest (aka October's Very Own) is one the most highly anticipated fests in the genre. Given Drake's audible love and loyalty to his stomping grounds, it comes as no surprise that the festival takes place in Toronto each summer. Over the years, the annual throw down has raked in all sorts of surprises: past performers include Eminem, Jay Z, Kanye West, Lauryn Hill, Stevie Wonder, Rihanna and other big names.",
+			link:"https://www.everfest.com/e/ovo-fest-toronto-on"
+		},
+		{
+			name: 'UNITY Festival',
+			price: "$",
+			video:"https://www.youtube.com/embed/Lc3tGyQ-HWg",
+			description:"4 days of incredible showcases of art and personal stories by local artists representing their communities. Headline performance by DāM-FunK at Dundas Square on Saturday, July 23rd!",
+			link:"http://unitycharity.com/"
+		},
+		{
+			name: 'TIME Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/QHY0qZzMKrg",
+			description:"TIME Festival is a one-day summer music festival held annually in Toronto, Ontario, Canada at Fort York Garrison Commons. The event focuses on electronic music, hip hop, and indie rock and features DJs and groups from around the world, as well as local Toronto artists.",
+			link:"https://www.everfest.com/e/time-festival-toronto-on"
+		},
+		{
+			name: 'NXNE',
+			price: "$$",
+			video:"https://www.youtube.com/embed/ZmJlF_2-zLM",
+			description:"NXNE is a raging annual 10-day summer festival and conference, featuring music, gaming, comedy, interactive and more at this packed cultural explosion.",
+			link:"http://nxne.com/"
+		},
+		{
+			name: 'Wayhome Festival',
+			price: "$$",
+			video:"https://www.youtube.com/embed/G5ZXSFIXQMw",
+			description:"WayHome Music & Arts, produced by Republic Live, is a three-day music and arts festival held at the custom-designed camping festival grounds, Burl’s Creek. The festival offers multiple stages, a variety of late-night spectacles and experiences, international and local cuisine, an on-site locally produced farmers market, original art installations - past artists include internally acclaimed artists such as; DoLab and Angus Watt, and more to 40,000+ fans over the course of the weekend.",
+			link:"http://wayhome.com/"
+		}
+	],
+	edm: [
+		{
+			name: 'VELD',
+			price: '$$',
+			video:"https://www.youtube.com/embed/ZT8DPAd82h4",
+			description:"The sixth annual Veld Music Festival, presented by INK Entertainment, Canada’s leading lifestyle and entertainment company, will heat up the summer music lineup on August 5th and 6th at Downsview Park in Toronto. Spanning across two days with over 30 internationally acclaimed Dance Music and Hip-Hop acts, this year’s Festival showcases some of the most revered names in electronic music. Prepare for Veld!",
+			link:"http://veldmusicfestival.com/"
+		},
+		{
+			name: 'Electric Island',
+			price: '$',
+			video:"https://www.youtube.com/embed/kaUBs-NfQsk",
+			description:"The 2017 season will feature international and local talent in an outdoor park side setting, overlooking Toronto’s waterfront and skyline. Officially closing each of Toronto’s four summer long weekends, this open air showcase is accessible via the Toronto Island Ferry.",
+			link:"http://electricisland.to/"
+		},
+		{
+			name: 'Dreams Music Festival',
+			price: '$$',
+			video:"https://www.youtube.com/embed/0eqNv2eqnSk",
+			description:"Digital Dreams Music Festival is a two-day electronic music festival held in Toronto, Ontario at the Flats at The Molson Canadian Amphitheatre. The festival offers four stages and has around 25,000 attendees over two days.",
+			link:"http://dreamsfestival.ca/"
+		}, 
+		{
+			name: 'TIME Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/QHY0qZzMKrg",
+			description:"TIME Festival is a one-day summer music festival held annually in Toronto, Ontario, Canada at Fort York Garrison Commons. The event focuses on electronic music, hip hop, and indie rock and features DJs and groups from around the world, as well as local Toronto artists.",
+			link:"https://www.everfest.com/e/time-festival-toronto-on"
+		},
+		{
+			name: 'NXNE',
+			price: "$$",
+			video:"https://www.youtube.com/embed/ZmJlF_2-zLM",
+			description:"NXNE is a raging annual 10-day summer festival and conference, featuring music, gaming, comedy, interactive and more at this packed cultural explosion.",
+			link:"http://nxne.com/"
+		},
+		{
+			name: 'Wayhome Festival',
+			price: "$$",
+			video:"https://www.youtube.com/embed/G5ZXSFIXQMw",
+			description:"WayHome Music & Arts, produced by Republic Live, is a three-day music and arts festival held at the custom-designed camping festival grounds, Burl’s Creek. The festival offers multiple stages, a variety of late-night spectacles and experiences, international and local cuisine, an on-site locally produced farmers market, original art installations - past artists include internally acclaimed artists such as; DoLab and Angus Watt, and more to 40,000+ fans over the course of the weekend.",
+			link:"http://wayhome.com/"
+		}
+	],
+	indie: [
+		{
+			name: 'Field Trip',
+			price: '$',
+			video:"https://www.youtube.com/embed/bQ1lgGQGiIo",
+			description:"Field Trip is about so much more than just great, world-class music. Food, art, comedy, family, fashion and a spirit of discovery are among our many inspirations and each year’s Field Trip is dedicated to collaborative explorations of these themes. ",
+			link:"http://fieldtriplife.com/"
+		},
+		{
+			name: 'Toronto Urban Roots Festival',
+			price: '$$',
+			video:"https://www.youtube.com/embed/RLkGLpl4cCc",
+			description:"The Toronto Urban Roots Festival will be closing out a jam-packed summer of music festivals in the city when it returns to Fort York Garrison Common for its fifth instalment this year.",
+			link:"http://torontourbanrootsfest.com/"
+		},
+		{
+			name: 'Indie Week',
+			price: '$',
+			video:"https://www.youtube.com/embed/zfHcpMNVeJQ",
+			description:"Indie Week is an annual music festival held over four days/nights, featuring local & international acts performing in the heart of Toronto, Canada. Showcases are judged by members of the music industry, and the top scoring act receives a paid trip (flights and accommodations for up to five band members) to headline INDIE WEEK EUROPE. Indie Week is an alternative, artist-friendly festival that supports emerging & upcoming artists.",
+			link:"http://www.indieweek.com/"
+		},		
+		{
+			name: 'TIME Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/QHY0qZzMKrg",
+			description:"TIME Festival is a one-day summer music festival held annually in Toronto, Ontario, Canada at Fort York Garrison Commons. The event focuses on electronic music, hip hop, and indie rock and features DJs and groups from around the world, as well as local Toronto artists.",
+			link:"https://www.everfest.com/e/time-festival-toronto-on"
+		},
+		{
+			name: 'NXNE',
+			price: "$$",
+			video:"https://www.youtube.com/embed/ZmJlF_2-zLM",
+			description:"NXNE is a raging annual 10-day summer festival and conference, featuring music, gaming, comedy, interactive and more at this packed cultural explosion.",
+			link:"http://nxne.com/"
+		},
+		{
+			name: 'Wayhome Festival',
+			price: "$$",
+			video:"https://www.youtube.com/embed/G5ZXSFIXQMw",
+			description:"WayHome Music & Arts, produced by Republic Live, is a three-day music and arts festival held at the custom-designed camping festival grounds, Burl’s Creek. The festival offers multiple stages, a variety of late-night spectacles and experiences, international and local cuisine, an on-site locally produced farmers market, original art installations - past artists include internally acclaimed artists such as; DoLab and Angus Watt, and more to 40,000+ fans over the course of the weekend.",
+			link:"http://wayhome.com/"
+		}
+	],
+	international: [
+		{
+			name: 'DesiFest',
+			price: '$',
+			video:"https://player.vimeo.com/video/216525112",
+			description:"We are a not-for-profit, grassroots organization established to foster and grow the large South Asian Arts Community in Toronto and across Canada. We help to unify, create awareness and develop new showcase opportunities in Toronto, across Canada and Internationally.",
+			link:"http://desifest.ca/"
+		},
+		{
+			name: 'Afrobeat Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/tiIQtIp0bdM",
+			description:"The event will be a celebration of the best of local, national and international music makers, brought together on one scorching hot stage. With performances by Afro Beats, Afro Pop, dancers and other musical stars, the audience won’t be able to resist the vibrant synergy of the variety of rhythms.",
+			link:"https://www.eventbrite.ca/e/afrobeat-2017-umoja-tickets-33039973450#"
+		},
+		{
+			name: 'Lulaworld Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/19tR6DwE70M?list=PLNgTCoJO3I3lhToqvyKwpUI5T6XJ5bOqg",
+			description:"Celebrating the fifteenth anniversary of the Toronto world music venue Lula Lounge, Lulaworld 2017 will once again bring together artists with roots in Cuba, Jamaica, Colombia, Mexico, Brazil, Portugal and beyond for 10 days of original collaborations and newest projects that cross borders, genres and generations.",
+			link:"http://www.lulaworld.ca/"
+		},
+	],
+	jazzBlues: [
+		{
+			name: 'Beaches International Jazz Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/27y10UO4jjw",
+			description:"The Beaches International Street festival is Toronto's #1 outdoor festival held annually. Jazz. Rhythm & Blues. Samba. Latin. 50 live bands. Outdoor music, entertainment, fun for the whole family and we bring you an exclusive original story about the people behind it.",
+			link:"http://beachesjazz.com/"
+		},
+		{
+			name: 'TD Toronto Jazz Festival',
+			price: '$',
+			video:"https://www.youtube.com/embed/Zh3_JI2TxV8",
+			description:"The Toronto Jazz Festival is a jazz event in Toronto which takes place for 10 days in late June through early July. Unlike the Toronto Beaches Jazz Festival, most of the events are indoors and located throughout the downtown core.",
+			link:"http://torontojazz.com/"
+		},
+		{
+	
+			name: 'Guelph Jazz Festival',
+			price: '$$',
+			description: 'Spearheaded by Ajay Heble, the Guelph Jazz Festival was founded in 1994 by a group of friends who shared a love of jazz and a commitment to Guelph. The Festival has consistently garnered praise from local, national, and international critics for its great programming. Guelph Jazz Festival offers adventurous music that inspires listeners to hear and see the world with a fresh perspective.',
+			video: 'https://www.youtube.com/watch?v=kAoQ4joqHfI',
+			link:'https://guelphjazzfestival.com'
+		}
+	]
+}
+
+$(function() {
+	//Allow user to select genre types (more than one accepted)
+	// User submits genre(s)
+	// Allow user to select between 2 price points ($ or $$)
+	// loop through each selected genre
+	// if price matches user input, push festival name into final selections array
+	// take final selections array and .uniq to remove duplicate festival names 
+	// Randomize and pick from final festival names 
+	// print landing page
+	// Randomize remaining options within price points
+	// Display final festival
+
+	$('.festForm').on('submit', function(e) {
+		e.preventDefault();
+		//Figure out what festival and price the user selected.
+		var selectedGenres = [];
+		var genreChoice = $('input[name=genre]:checked').each(function(genre){	
+			selectedGenres.push($(this).val());
+		});
+
+		var budget = $('input[name=budget]:checked').val();	
+
+		var userFestivals = [];
+		selectedGenres.forEach(function(genre){
+			userFestivals.push(festivals[genre]);
+		});
+
+		var merged = [].concat.apply([], userFestivals);
+
+		var refinedUserFestivals = [];
+		refinedUserFestivals = _.groupBy(merged, function(genre) {
+			return genre;
+		});
+		
+		var recurringResults = [];
+
+		for( var festival in refinedUserFestivals) {
+			refinedUserFestivals[festival].map(function(item){
+				recurringResults.push({
+					price: item.price,
+					name: item.name,
+					description: item.description,
+					video: item.video,
+					link: item.link,
+				})
+			})
+			
+		}
+
+		recurringResults.sort(function(a, b){
+			return b.count - a.count;
+		});
+
+		var onlyMostPopularFestivals = recurringResults.filter(function(festival) {
+			return festival.count === recurringResults[0].count;
+		});
+
+		var filteredBudget = onlyMostPopularFestivals.filter(function(festival){
+			return festival.price === budget;
+		});
+
+		//gettting a random index from the filteredBudeget Array	
+		var randomIndex = Math.floor(Math.random() * filteredBudget.length);
+
+		// console.log(filteredBudget[randomIndex]);
+		finalPage(filteredBudget[randomIndex])
+	});
+});
+
+
+function finalPage(filteredOptions) {
+		var myTemplate = $("#finalPageTemplate").html();
+		var compiledTemplate = Handlebars.compile(myTemplate);
+		$('.festivalsResults').append(compiledTemplate(filteredOptions) );
+};
+
+$(document).ready(function () {
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top
+         }, 900, 'swing', function () {
+             window.location.hash = target;
+         });
+     });
+	$(".submit").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#festivalsResults").offset().top
+	    }, 2000);
+	});
+ });
+
+
+
+
+
+
+
+
